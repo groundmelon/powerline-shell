12703e492a515f64a12d1f9de5c3599be4ad9fba
LIU Tianbo <liutianbocn@gmail.com>
2016-10-10 13:27:51 +0800

    modify ros

--- segments/ros_master.py
+++ segments/ros_master.py
@@ -2,11 +2,14 @@
 def add_ros_master_segment(powerline):
     import os
     uri = os.getenv('ROS_MASTER_URI')
-    if uri.find('tegra-ubuntu')!=-1:
-        msg = 'RmtROS'
+    if uri is not None and uri.find('localhost')==-1:
+        rst = re.match('http://(.*):11311', uri)
+        if rst is None:
+            msg = "ParseRosFail"
+        else:
+            msg = rst.groups()[0]
         fgcolor = Color.SSH_FG
-        bgcolor = Color.SSH_BG
+        bgcolor = 129
         powerline.append(msg, fgcolor, bgcolor)
     else:
         return
-
